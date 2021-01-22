b71d5ddcd5f2fa4c5b334acddf0f029c99d58d8e
Dennis Duncan <DDuncan@qa.com>
2021-01-22 10:29:54 +0000

    Edited CRUD Functionality of Item

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -26,31 +26,42 @@ public class ItemController implements ICrudController<Item> {
     public List<Item> readAll() {
         List<Item> items = itemDao.readAll();
         for (Item item : items) {
-            LOGGER.info(item);
+            LOGGER.info(item.toString());
         }
         return items;
     }
 
     @Override
     public Item create() {
-        LOGGER.info("Please enter an item name");
-        String itemName = javaUtilities.getString();
-        LOGGER.info("Please the value of the item");
-        Double itemValue = javaUtilities.getDouble();
-        Item item = itemDao.create(new Item(itemName, itemValue));
+        LOGGER.info("Please enter an item");
+        String name = javaUtilities.getString();
+        LOGGER.info("Please enter an item value");
+        double value = javaUtilities.getDouble();
+        Item item = itemDao.create(new Item(name, value));
         LOGGER.info("Item created");
         return item;
     }
 
-	@Override
-	public Item update() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Item update() {
+        LOGGER.info("Please enter the id of the item you would like to update");
+        Long id = javaUtilities.getLong();
+        LOGGER.info("Please enter an item name");
+        String name = javaUtilities.getString();
+        LOGGER.info("Please enter an item value");
+        double value = javaUtilities.getDouble();
+        Item item = itemDao.update(new Item(id, name, value));
+        LOGGER.info("Item Updated");
+        return item;
+    }
+	
 
-	@Override
-	public int delete() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int delete() {
+        LOGGER.info("Please enter the id of the customer you would like to delete");
+        Long id = javaUtilities.getLong();
+        return itemDao.delete(id);
+
 }
+}
+
--- src/main/java/com/qa/ims/persistence/dao/ItemDao.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,53 +14,117 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.DatabaseUtilities;
 
-public class ItemDao implements IDomainDao<Item> { 
+public class ItemDao implements IDomainDao<Item> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+   
+    @Override
+	public Item create(Item item) {
+		try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO items (name, value) VALUES (?,?)");) {
+			statement.setString(1, item.getname());
+			statement.setDouble(2, item.getvalue());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+        
+		}
+		return null;
+
+}
+
+
+    public Item read(Long id) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Item> readAll() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+            List<Item> items = new ArrayList<>();
+            while (resultSet.next()) {
+                items.add(modelFromResultSet(resultSet));
+            }
+            return items;
+        } catch (SQLException e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return new ArrayList<>();
+    }
+
+    public Item readLatest() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
 	
-	public static final Logger LOGGER = LogManager.getLogger();
 	
+
+
 	 @Override
-	    public Item create(Item item) {
+	    public Item update(Item item) {
 	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
 	                PreparedStatement statement = connection
-	                        .prepareStatement("INSERT INTO items(item_name, item_value) VALUES (?, ?)");) {
-	            statement.setString(1, item.getItemName());
-	            statement.setDouble(2, item.getItemValue());
+	                        .prepareStatement("UPDATE items SET name = ?, value = ? WHERE id = ?");) {
+	            statement.setString(1, item.getname());
+	            statement.setDouble(2, item.getvalue());
+	            statement.setLong(3, item.getId());
 	            statement.executeUpdate();
-	            return readLatest();
+	            return read(item.getId());
 	        } catch (Exception e) {
 	            LOGGER.debug(e);
 	            LOGGER.error(e.getMessage());
 	        }
-			return null;
-
-}
-
-	public Item readLatest() {
-		// TODO Auto-generated method stub
-		return null;
+	        return null;
 	}
 
-	@Override
-	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Item update(Item t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	 @Override
+	    public int delete(long id) {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                Statement statement = connection.createStatement();) {
+	            return statement.executeUpdate("delete from items where id = " + id);
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return 0;
+		}
 
-	@Override
-	public int delete(long id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	 @Override
+		public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		        Long id = resultSet.getLong("id");
+		        String name = resultSet.getString("name");
+		        double value = resultSet.getDouble("value");
+		        return new Item(id, name, value);
 
-	@Override
-	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		        
+		        
+	 }
+	 
+	 
 }
