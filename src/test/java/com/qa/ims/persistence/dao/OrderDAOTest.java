892f02c01837567fa59ffe6fe0c81c68aafbf09e
Dennis Duncan <DDuncan@qa.com>
2021-01-29 10:18:41 +0000

    Improved coverage on Orders

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -14,75 +14,73 @@ import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DatabaseUtilities;
 
 public class OrderDAOTest {
-	
+	private Order order1;
 	private Item Laptop = new Item(1L, "name", 500.00);
 	private Item item1 = new Item(2L, "name1", 1.00);
 	private Item item2 = new Item(3L, "name2", 2.00);
 	private final OrderDao DAO = new OrderDao(new ItemDao(), new CustomerDao());
-	
+
 	@Before
 	public void setup() {
-		 DatabaseUtilities.connect();
-	        DatabaseUtilities.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
-	        List<Item> itemList = new ArrayList<>();
-	        itemList.add(new Item(1L, "Laptop", 500.00));
-	        itemList.add(new Item(2L, "item1", 1.00));
-	        
-	        Order order1 = new Order(1L, new Customer(1L, "jordan", "harrison"), 501.00, itemList);
-	        
-	    }
+		DatabaseUtilities.connect();
+		DatabaseUtilities.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+		List<Item> itemList = new ArrayList<>();
+		itemList.add(new Item(1L, "Laptop", 500.00));
+		itemList.add(new Item(2L, "item1", 1.00));
 
-@Test
-public void testCreate() {
-	final Order created = new Order(2L, new Customer(1L, "jordan", "harrison"), 0.0);
-	assertEquals(created, DAO.create(created));
-	
-}
+		Order order1 = new Order(1L, new Customer(1L, "jordan", "harrison"), 501.00, itemList);
 
-@Test
-public void testReadAll() {
-	List<Order> expected = new ArrayList<>();
-	expected.add(new Order(1L, new Customer(1L, "jordan", "harrison"), 501.0));
-	assertEquals(expected, DAO.readAll());
-	
-}
-	
+	}
 
+	@Test
+	public void testCreate() {
+		final Order created = new Order(4L, new Customer(1L, "jordan", "harrison"), 0.0);
+		assertEquals(created, DAO.create(created));
+
+	}
+
+	@Test
+	public void testReadAll() {
+		List<Order> expected = new ArrayList<>();
+		expected.add(new Order(1L, new Customer(1L, "jordan", "harrison"), 501.0));
+		expected.add(new Order(2L, new Customer(1L, "jordan", "harrison"), 0.0));
+		expected.add(new Order(3L, new Customer(1L, "jordan", "harrison"), 0.0));
+		assertEquals(expected, DAO.readAll());
+
+	}
 
 	@Test
 	public void testReadLatest() {
-		List<Order> expected = new ArrayList<>();
-		expected.add(new Order(1L, new Customer(1L, "jordan", "harrison"), 501.0));
+		final Order expected = new Order(3L, new Customer(1L, "jordan", "harrison"), 0.0);
 		assertEquals(expected, DAO.readLatest());
-		
+
 	}
-
-
 
 	@Test
 	public void testUpdate() {
-	assertEquals(null, DAO.update(null));
+		assertEquals(null, DAO.update(null));
 
 	}
-	
+
 	@Test
 	public void testDelete() {
 		assertEquals(1L, DAO.delete(1L));
 
 	}
+
+	@Test
+	public void testreadOrder() {
+		final long ID = 2L;
+		final Order expected = new Order(2L, new Customer(1L, "jordan", "harrison"), 0.0);
+		assertEquals(expected, DAO.readOrder(ID));
+
+	}
+
+	@Test
+	public void testUpdateAdd() {
+		final Order expected = new Order(4L, new Customer(1L, "jordan", "harrison"), 0.0);
+        assertEquals(expected, DAO.update(order1));
+
+	}
 	
 }
-		
-//	@Test
-//	public void testreadOrder() {
-//		final long Id = 1L;
-//		assertEquals(new Order (ID,L, 1L), DAO.readOrder(ID));
-	
-
-//	@Test
-//	public void testUpdateAdd(Order) {
-//		assertEquals(Order, DAO.update(Order));
-//	}
-//	
-//	
-//}
--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -2,4 +2,6 @@ INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `items` (`name` , `value`) VALUES ('Laptop', 500.00);
 INSERT INTO `items` (`name` , `value`) VALUES ('item1', 1.00);
 INSERT INTO `orders` (`fk_customers_id`, `order_value`) VALUES (1, 501.00);
-INSERT INTO `orders_items` (`fk_orders_id`, `fk_items_id`) VALUES (1,0);
+INSERT INTO `orders` (`fk_customers_id`, `order_value`) VALUES (1, 0.0);
+INSERT INTO `orders` (`fk_customers_id`, `order_value`) VALUES (1, 0.0);
+INSERT INTO `orders_items` (`fk_orders_id`, `fk_items_id`) VALUES (1, 0);
